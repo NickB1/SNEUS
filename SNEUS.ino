8c23a4bb3aec544a8b44a33b748cdd450edc91b5
NickB <nick.blommen1@gmail.com>
2018-08-30 21:17:03 +0200

    Cleanup

--- SNEUS.ino
+++ SNEUS.ino
@@ -11,10 +11,10 @@
 #define WEBSITE_PRINT_FREQ  20
 #define WEBSITE_PRINT_ON    1
  
-#define RTTY_ASCII 8          // RTTY_ASCII 7 or 8
+#define RTTY_ASCII    8       // RTTY_ASCII 7 or 8
 #define RTTY_STOPBITS 2       // Either 1 or 2
-#define RTTY_TXDELAY 0        // Delay between sentence TX's
-#define RTTY_BAUD 300         // Baud rate for use with RFM22B Max = 600
+#define RTTY_TXDELAY  0       // Delay between sentence TX's
+#define RTTY_BAUD     300     // Baud rate for use with RFM22B Max = 600
 
 #define RFM22B_SDN 8
 #define RFM22B_REINIT_CNT 8
@@ -92,12 +92,10 @@ int   rfm22_reinitcntr = 0;
 void setup()
 { 
   pinMode(LED,OUTPUT);
-  digitalWrite(LED, LOW);
-  delay(500);
-  digitalWrite(LED, HIGH);
-  delay(500);
+  
+  blink_led(1);
     
-  initialise_timer_interrupt();
+  init_timer_interrupt();
   
   Serial.begin(9600);
   
@@ -120,17 +118,126 @@ void setup()
   strcat(website_string,"\n");
   
   delay(1000);
+  
   //Setup RFM22B
-  setupRadio();
+  init_rfm22();
 
-  blink_success();
+  blink_led(4);
 }
  
 void loop()
+{  
+  if(parse_gps())
+  {
+    measure_battery();
+    build_telem_string();
+  }
+
+  //Reinitialisation of RFM22
+  if(rfm22_reinit == true)
+  {
+    init_rfm22();
+    rfm22_reinit = false;
+  }
+}
+
+void blink_led(uint8_t count)
 {
-  unsigned long chars;
-  unsigned short sentences, failed;
-  
+  for(uint8_t i = 0; i < count; i++)
+  {
+    digitalWrite(LED, LOW);
+    delay(500);
+    digitalWrite(LED, HIGH); 
+    delay(500);
+  }
+}
+
+void init_rfm22(void)
+{
+  pinMode(RFM22B_SDN, OUTPUT);    // RFM22B SDN is on ARDUINO A3
+  digitalWrite(RFM22B_SDN, LOW);
+  delay(1000);
+  rfm22::initSPI();
+  radio1.init();
+  radio1.write(0x71, 0x00); // unmodulated carrier
+  //This sets up the GPIOs to automatically switch the antenna depending on Tx or Rx state, only needs to be done at start up
+  radio1.write(0x0b,0x12);
+  radio1.write(0x0c,0x15);
+  radio1.setFrequency(RFM22_FREQUENCY);
+  radio1.write(0x6D, 0x04);// turn tx low power 11db
+  radio1.write(0x07, 0x08);
+  delay(500);
+}
+
+// Send a byte array of UBX protocol to the GPS
+void sendUBX(uint8_t *MSG, uint8_t len) {
+  for(int i=0; i<len; i++) {
+    Serial.write(MSG[i]);
+    //mySerial.print(MSG[i], HEX);
+  }
+  Serial.println();
+}
+
+// Calculate expected UBX ACK packet and parse UBX response from GPS
+boolean getUBX_ACK(uint8_t *MSG) {
+  uint8_t b;
+  uint8_t ackByteID = 0;
+  uint8_t ackPacket[10];
+  unsigned long startTime = millis();
+  //mySerial.print(" * Reading ACK response: ");
+ 
+  // Construct the expected ACK packet    
+  ackPacket[0] = 0xB5;  // header
+  ackPacket[1] = 0x62;  // header
+  ackPacket[2] = 0x05;  // class
+  ackPacket[3] = 0x01;  // id
+  ackPacket[4] = 0x02;  // length
+  ackPacket[5] = 0x00;
+  ackPacket[6] = MSG[2];  // ACK class
+  ackPacket[7] = MSG[3];  // ACK id
+  ackPacket[8] = 0;   // CK_A
+  ackPacket[9] = 0;   // CK_B
+ 
+  // Calculate the checksums
+  for (uint8_t i=2; i<8; i++) {
+    ackPacket[8] = ackPacket[8] + ackPacket[i];
+    ackPacket[9] = ackPacket[9] + ackPacket[8];
+  }
+ 
+  while (1) {
+ 
+    // Test for success
+    if (ackByteID > 9) {
+      // All packets in order!
+      //mySerial.println(" (SUCCESS!)");
+      return true;
+    }
+ 
+    // Timeout if no valid response in 3 seconds
+    if (millis() - startTime > 3000) { 
+      //mySerial.println(" (FAILED!)");
+      return false;
+    }
+ 
+    // Make sure data is available to read
+    if (Serial.available()) {
+      b = Serial.read();
+ 
+      // Check that bytes arrive in sequence as per expected ACK packet
+      if (b == ackPacket[ackByteID]) { 
+        ackByteID++;
+        //mySerial.print(b, HEX);
+      } 
+      else {
+        ackByteID = 0;  // Reset and look again, invalid order
+      }
+ 
+    }
+  }
+}
+
+uint8_t parse_gps(void)
+{
   while(Serial.available())
   {
   char c = Serial.read();
@@ -165,110 +272,40 @@ void loop()
     minute = ((time - (hour * 1000000)) / 10000);
     second = ((time - ((hour * 1000000) + (minute * 10000))));
     second = second / 100;
-    
-    //Read Battery Voltage        
-    bat_sensorValue = analogRead(BAT_SENSE_PIN);                    
-    bat_actualValue = (bat_sensorValue / 1023.00) * bat_divider;
-    bat_v1 = bat_actualValue;
-    bat_v2 = (bat_actualValue - bat_v1) * 100;
-    snprintf(bat_voltage, 30, "%i.%02i", bat_v1, bat_v2);
-    
-    //Build new data string
-    cBusy = true;
-    sprintf(tracker_string,"$$$$%s,%li,%02i:%02i:%02i,%s,%s,%s,%i,%s,",callsign,packet_cnt,hour,minute,second,latbuf,lonbuf,altbuf,sats,bat_voltage); 
-    sprintf(sensor_string,"%i",geiger_slowcpm);
-    strcat(tracker_string,sensor_string);
-    unsigned int CHECKSUM = gps_CRC16_checksum(tracker_string);  // Calculates the checksum for this tracker_string
-    
-    sprintf(checksum_str, "*%04X\n", CHECKSUM);
-    strcat(tracker_string,checksum_str);
-    cBusy = false;
-    
-    gps.stats(&chars, &sentences, &failed);
   }
   else
   {
     digitalWrite(LED,LOW);
   }
 
-  //Reinitialisation of RFM22
-  if(rfm22_reinit == true)
-  {
-    setupRadio();
-    rfm22_reinit = false;
-  }
-}
- 
-void geiger_pulse(void)
-{
-    if (geiger_count < UINT16_MAX)  // check for overflow, if we do overflow just cap the counts at max possible
-    geiger_count++; // increase event counter
-    
-    geiger_eventflag = 1; // tell main program loop that a GM pulse has occurred
+  return newData;
 }
 
-void geiger_1s_calc(void)
+void measure_battery(void)
 {
-    uint8_t i, x;  // index for fast mode
-    
-    geiger_cps = geiger_count;
-    geiger_slowcpm -= geiger_buffer[geiger_idx];   // subtract oldest sample in sample buffer
-    
-    if (geiger_count > UINT8_MAX) { // watch out for overflowing the sample buffer
-      geiger_count = UINT8_MAX;
-      geiger_overflow = 1;
-    }
-        
-    geiger_slowcpm += geiger_count;   // add current sample
-    geiger_buffer[geiger_idx] = geiger_count;  // save current sample to buffer (replacing old value)
-    
-    // Compute CPM based on the last SHORT_PERIOD samples
-    geiger_fastcpm = 0;
-    for(i=0; i<GEIGER_SHORT_PERIOD;i++) {
-      x = geiger_idx - i;
-      if (x < 0)
-        x = GEIGER_LONG_PERIOD + x;
-      geiger_fastcpm += geiger_buffer[x]; // sum up the last 5 CPS values
-    }
-    geiger_fastcpm = geiger_fastcpm * (GEIGER_LONG_PERIOD/GEIGER_SHORT_PERIOD); // convert to CPM
-    
-    // Move to the next entry in the sample buffer
-    geiger_idx++;
-    if (geiger_idx >= GEIGER_LONG_PERIOD)
-      geiger_idx = 0;
-    geiger_count = 0;  // reset counter
+  //Read Battery Voltage        
+    bat_sensorValue = analogRead(BAT_SENSE_PIN);                    
+    bat_actualValue = (bat_sensorValue / 1023.00) * bat_divider;
+    bat_v1 = bat_actualValue;
+    bat_v2 = (bat_actualValue - bat_v1) * 100;
+    snprintf(bat_voltage, 30, "%i.%02i", bat_v1, bat_v2);
 }
 
- 
-void setupRadio(){
-  pinMode(RFM22B_SDN, OUTPUT);    // RFM22B SDN is on ARDUINO A3
-  digitalWrite(RFM22B_SDN, LOW);
-  delay(1000);
-  rfm22::initSPI();
-  radio1.init();
-  radio1.write(0x71, 0x00); // unmodulated carrier
-  //This sets up the GPIOs to automatically switch the antenna depending on Tx or Rx state, only needs to be done at start up
-  radio1.write(0x0b,0x12);
-  radio1.write(0x0c,0x15);
-  radio1.setFrequency(RFM22_FREQUENCY);
-  radio1.write(0x6D, 0x04);// turn tx low power 11db
-  radio1.write(0x07, 0x08);
-  delay(500);
-}
-
-void rtty_txbit (int bit)
+void build_telem_string(void)
 {
-  if (bit)
-  {
-    radio1.write(0x73,0x03); // High
-  }
-  else
-  {
-    radio1.write(0x73,0x00); // Low
-  }
+    //Build new data string
+    cBusy = true;
+    sprintf(tracker_string,"$$$$%s,%li,%02i:%02i:%02i,%s,%s,%s,%i,%s,",callsign,packet_cnt,hour,minute,second,latbuf,lonbuf,altbuf,sats,bat_voltage); 
+    sprintf(sensor_string,"%i",geiger_slowcpm);
+    strcat(tracker_string,sensor_string);
+    unsigned int CHECKSUM = calc_crc16_checksum(tracker_string);  // Calculates the checksum for this tracker_string
+    
+    sprintf(checksum_str, "*%04X\n", CHECKSUM);
+    strcat(tracker_string,checksum_str);
+    cBusy = false;
 }
  
-uint16_t gps_CRC16_checksum (char *string)
+uint16_t calc_crc16_checksum(char *string)
 {
   size_t i;
   uint16_t crc;
@@ -284,8 +321,9 @@ uint16_t gps_CRC16_checksum (char *string)
   }
  
   return crc;
-}   
-void initialise_timer_interrupt()
+}
+
+void init_timer_interrupt(void)
 {
   // initialize Timer1
   cli();          // disable global interrupts
@@ -299,86 +337,6 @@ void initialise_timer_interrupt()
   // enable timer compare interrupt:
   TIMSK1 |= (1 << OCIE1A);
   sei();          // enable global interrupts
-}
-
-// Send a byte array of UBX protocol to the GPS
-void sendUBX(uint8_t *MSG, uint8_t len) {
-  for(int i=0; i<len; i++) {
-    Serial.write(MSG[i]);
-    //mySerial.print(MSG[i], HEX);
-  }
-  Serial.println();
-}
-
-// Calculate expected UBX ACK packet and parse UBX response from GPS
-boolean getUBX_ACK(uint8_t *MSG) {
-  uint8_t b;
-  uint8_t ackByteID = 0;
-  uint8_t ackPacket[10];
-  unsigned long startTime = millis();
-  //mySerial.print(" * Reading ACK response: ");
- 
-  // Construct the expected ACK packet    
-  ackPacket[0] = 0xB5;	// header
-  ackPacket[1] = 0x62;	// header
-  ackPacket[2] = 0x05;	// class
-  ackPacket[3] = 0x01;	// id
-  ackPacket[4] = 0x02;	// length
-  ackPacket[5] = 0x00;
-  ackPacket[6] = MSG[2];	// ACK class
-  ackPacket[7] = MSG[3];	// ACK id
-  ackPacket[8] = 0;		// CK_A
-  ackPacket[9] = 0;		// CK_B
- 
-  // Calculate the checksums
-  for (uint8_t i=2; i<8; i++) {
-    ackPacket[8] = ackPacket[8] + ackPacket[i];
-    ackPacket[9] = ackPacket[9] + ackPacket[8];
-  }
- 
-  while (1) {
- 
-    // Test for success
-    if (ackByteID > 9) {
-      // All packets in order!
-      //mySerial.println(" (SUCCESS!)");
-      return true;
-    }
- 
-    // Timeout if no valid response in 3 seconds
-    if (millis() - startTime > 3000) { 
-      //mySerial.println(" (FAILED!)");
-      return false;
-    }
- 
-    // Make sure data is available to read
-    if (Serial.available()) {
-      b = Serial.read();
- 
-      // Check that bytes arrive in sequence as per expected ACK packet
-      if (b == ackPacket[ackByteID]) { 
-        ackByteID++;
-        //mySerial.print(b, HEX);
-      } 
-      else {
-        ackByteID = 0;	// Reset and look again, invalid order
-      }
- 
-    }
-  }
-}
-
-void blink_success(void)
-{
-  digitalWrite(LED, LOW);
-  delay(500);
-  digitalWrite(LED, HIGH); 
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
-  digitalWrite(LED, HIGH); 
-  delay(500);
-  digitalWrite(LED, LOW);
 }
 
 ISR(TIMER1_COMPA_vect)
@@ -478,5 +436,57 @@ ISR(TIMER1_COMPA_vect)
     int_count=0;
     geiger_1s_calc();
   }
+}
+
+void rtty_txbit (int bit)
+{
+  if (bit)
+  {
+    radio1.write(0x73,0x03); // High
+  }
+  else
+  {
+    radio1.write(0x73,0x00); // Low
+  }
+}
+
+void geiger_pulse(void)
+{
+    if (geiger_count < UINT16_MAX)  // check for overflow, if we do overflow just cap the counts at max possible
+    geiger_count++; // increase event counter
+    
+    geiger_eventflag = 1; // tell main program loop that a GM pulse has occurred
+}
+
+void geiger_1s_calc(void)
+{
+    uint8_t i, x;  // index for fast mode
+    
+    geiger_cps = geiger_count;
+    geiger_slowcpm -= geiger_buffer[geiger_idx];   // subtract oldest sample in sample buffer
+    
+    if (geiger_count > UINT8_MAX) { // watch out for overflowing the sample buffer
+      geiger_count = UINT8_MAX;
+      geiger_overflow = 1;
+    }
+        
+    geiger_slowcpm += geiger_count;   // add current sample
+    geiger_buffer[geiger_idx] = geiger_count;  // save current sample to buffer (replacing old value)
+    
+    // Compute CPM based on the last SHORT_PERIOD samples
+    geiger_fastcpm = 0;
+    for(i=0; i<GEIGER_SHORT_PERIOD;i++) {
+      x = geiger_idx - i;
+      if (x < 0)
+        x = GEIGER_LONG_PERIOD + x;
+      geiger_fastcpm += geiger_buffer[x]; // sum up the last 5 CPS values
+    }
+    geiger_fastcpm = geiger_fastcpm * (GEIGER_LONG_PERIOD/GEIGER_SHORT_PERIOD); // convert to CPM
+    
+    // Move to the next entry in the sample buffer
+    geiger_idx++;
+    if (geiger_idx >= GEIGER_LONG_PERIOD)
+      geiger_idx = 0;
+    geiger_count = 0;  // reset counter
 }
 
